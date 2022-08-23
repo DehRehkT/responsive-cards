401ae9e8fe2976589441e9c5055868ce246da558
Derek Mundondo <mundondodt@gmail.com>
2022-08-23 21:56:16 +0200

    Responsive card designs first commit.

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <div className="main-container">
+        <div className="heading">
+          <h1 className="heading_title">My Card Designs</h1>          
+        </div>
+        <div className="cards">
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>   
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div> 
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>   
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>  
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>   
+          <div className="card card-1">                          
+            <ul>
+              <li>1 email address</li>
+              <li>1GB of cloud storage</li>
+              <li>Realtime Backup</li>
+              <li>End-to-end encryption</li>
+              <li>Access to our mobile apps</li>
+              <li>Access to our mobile apps</li>
+            </ul>                     
+          </div>                  
+        </div>
+      </div>  
+    </div>
+  );
+}
+
+export default App;
